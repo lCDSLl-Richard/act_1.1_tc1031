db06be98e93481f27530da29f3a152108ddde275
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2022-08-23 08:44:41 -0500

    Activity solved. Documentation added

--- activity.h
+++ activity.h
@@ -1,8 +1,8 @@
 // =================================================================
 //
 // File: activity.h
-// Author:
-// Date:
+// Author: Ricardo Fernández
+// Date: 23/08/2022
 //
 // =================================================================
 #ifndef ACTIVITY_H
@@ -14,10 +14,16 @@
 //
 // @param n	The maximum limit of the sum.
 // @return	The result of the addition from 1 to n.
-// @Complexity	??
+// @Complexity	O(n)
 // =================================================================
-unsigned int sumaIterativa(unsigned int n) {
-	return 0;
+unsigned int sumaIterativa(unsigned int n)
+{
+	unsigned int suma = 0;
+
+	for (int i = 1; i <= n; i++)
+		suma += i;
+
+	return suma;
 }
 
 // =================================================================
@@ -26,10 +32,11 @@ unsigned int sumaIterativa(unsigned int n) {
 //
 // @param n	The maximum limit of the sum.
 // @return	The result of the addition from 1 to n.
-// @Complexity	??
+// @Complexity	O(n)
 // =================================================================
-unsigned int sumaRecursiva(unsigned int n) {
-	return 0;
+unsigned int sumaRecursiva(unsigned int n)
+{
+	return n == 1 ? 1 : n + sumaRecursiva(n - 1);
 }
 
 // =================================================================
@@ -38,10 +45,11 @@ unsigned int sumaRecursiva(unsigned int n) {
 //
 // @param n	The maximum limit of the sum.
 // @return	The result of the addition from 1 to n.
-// @Complexity	??
+// @Complexity	O(1)
 // =================================================================
-unsigned int sumaDirecta(unsigned int n) {
-	return 0;
+unsigned int sumaDirecta(unsigned int n)
+{
+	return (n * (n + 1)) / 2;
 }
 
 #endif /* ACTIVITY_H */
